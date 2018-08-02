caefab96b9a5dcf8c62bd3e340af1806b309cb20
E. Virtanen <33025711+currentco@users.noreply.github.com>
2018-08-02 10:34:32 +0300

    V 1.3

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -179,3 +179,10 @@ class DBHelper {
 
 }
 
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('/sw.js')
+           .then(function() { 
+            console.log("Service Worker Registered"); });
+}
+
